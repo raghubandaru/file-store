95d82a55392496a50563802ee4df471e374c10d5
Raghu Bandaru <raghubandaru45@gmail.com>
2026-03-06 01:39:11 +0530

    Modify page content for deployment

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@file-store/design-system';
 export default function Home() {
   return (
     <div>
-      <h1>This Button component is served from design system package</h1>
+      <h1>This is design system Button component</h1>
       <Button>Click here</Button>
     </div>
   );
