9f948d8c47320b578cb5171f620d0e14e5dba919
Raghu Bandaru <raghubandaru45@gmail.com>
2026-03-07 22:31:13 +0530

    Fix top nav styling

--- apps/web/src/features/components/LogoutButton/LogoutButton.tsx
+++ apps/web/src/features/components/LogoutButton/LogoutButton.tsx
@@ -3,7 +3,7 @@ import { Button, Form } from "@file-store/design-system";
 
 export default function LogoutButton() {
   return (
-    <Form action={logoutAction}>
+    <Form action={logoutAction} className="inlineBlock">
       <Button type="submit">Logout</Button>
     </Form>
   );
